37acbb6b312be2a3deb7cf0e5378a6fa6caed6cd
alfathmuqoddas <alfath.muqoddas@gmail.com>
2022-01-10 00:35:19 +0700

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "Gatsby Contentful Blog",
+  },
+  plugins: [
+    {
+      resolve: "gatsby-source-contentful",
+      options: {
+        accessToken: "qLBPjhcrcIbBC6k2RqW5VM58_EuFzLzIzMGz72tr7fs",
+        spaceId: "zwrjdklfi3qu",
+      },
+    },
+    "gatsby-plugin-image",
+    "gatsby-transformer-remark",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: "./src/pages/",
+      },
+      __key: "pages",
+    },
+  ],
+};
